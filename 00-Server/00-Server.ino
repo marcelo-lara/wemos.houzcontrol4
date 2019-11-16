61e983c3dc33eda280ac27df111ef3914b53c267
Marcelo Lara <marcelo@darkblue.tv>
2019-11-15 21:31:48 -0300

    server: add api declarations

--- 00-Server/00-Server.ino
+++ 00-Server/00-Server.ino
@@ -35,16 +35,58 @@ void runTask(){
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // API
 void apiSetup(){
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        //TODO: single ui to handle devices
-        request->send(200, "text/plain", "HouzNode");
-    }); 
 
 
-    server.onNotFound(notFound);
-    server.begin();
+// frontend
+  if (SPIFFS.begin()){
+    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+  }
+  else{
+    Serial.println("An Error has occurred while mounting SPIFFS");
+  }
+
+//enable cors
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+  DefaultHeaders::Instance().addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  DefaultHeaders::Instance().addHeader("Pragma", "no-cache");
+  DefaultHeaders::Instance().addHeader("Expires", "0");
+  DefaultHeaders::Instance().addHeader("Server", "HouzControl/4.0.0");
+
+
+  // -> node status
+  server.on("/api/status", HTTP_POST, nullRequest, NULL, api_setNodeStatus);
+
+  // -> node update
+  server.on("/api/update", HTTP_POST, nullRequest, NULL, api_updateNodeStatus);
+
+  // -> status request
+  server.on("/api", HTTP_GET, api_pingReply);
+
+  // <- rf send request
+  server.on("/api/send", HTTP_POST, nullRequest, NULL, api_sendRfRequest);
+
+  // <- node status    
+  server.on("/api", HTTP_POST, nullRequest, NULL, api_serverStatus);
+
+  server.onNotFound(notFound);
+  server.begin();
 
 };
+
+// replace node devices with received values
+void api_setNodeStatus(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){}
+
+// update node device with received values
+void api_updateNodeStatus(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){}
+
+// request to deliver a RF packet
+void api_sendRfRequest(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){}
+
+// response with all nodes and devices
+void api_serverStatus(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){}
+
+// response to node keepalive query
+void api_pingReply(AsyncWebServerRequest *request){}
 
 void notFound(AsyncWebServerRequest *request) {request->send(404, "text/plain", "Not found");}
 void nullRequest(AsyncWebServerRequest *request){};
