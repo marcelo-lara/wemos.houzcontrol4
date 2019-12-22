27a4321ef4dcad60f15830148c2d4d8627143473
Marcelo Lara <marcelo@darkblue.tv>
2019-12-21 23:09:20 -0300

    device model updates

--- 00-Server/src/Core/Model.h
+++ 00-Server/src/Core/Model.h
@@ -2,30 +2,49 @@
 #include "Arduino.h"
 
 
+
+
 ///////////////////////////////////////////////////////////////////////////
 // DEVICE
+enum DeviceType {
+  devtype_light = 1,
+  devtype_env   = 4,
+  devtype_fan   = 2,
+  devtype_ac    = 3,
+  
+};
 
 struct Device {
 public:
   u8  id;       //channel
   u8  node;     //node
   u32 payload;  //value
+  u8  type;     //device type
 };
 
-struct Fan: Device {
-  bool on;
-  u8   speed;
-};
-
-struct Ac: Device {
-  bool on;
-  bool fanOn;
-  u8   temp;
+struct Environment: Device {
+  bool  on;     //sensor is on
+  float temp;   //temperature
+  float hum;    //humidity
+  float press;  //pressure
+  float light;  //light level
+  
 };
 
 struct Light: Device {
-  bool on;
-  u8   muxId; //id of multpiplexed out
+  bool on;      //light status
+  u8   muxId;   //id of multpiplexed out
+};
+
+struct Fan: Device {
+  bool on;      //fan is on, if fan is off speed is 0
+  u8   speed;   //when on, fan speed
+};
+
+struct Ac: Device {
+  bool on;      //ac is on
+  bool fanOn;   //ac fan is on
+  u8   temp;    //ac temperature
 };
 
 ///////////////////////////////////////////////////////////////////////////
@@ -33,19 +52,20 @@ struct Light: Device {
 
 // Task Command
 enum Command{
-    command_set_device  = 1,
-    command_play_scene  = 2,
-    command_set_scene   = 3,
-    command_rf_send     = 4, 
-    command_ir_send     = 5,
-    command_scene_pause = 9
+  command_set_device  =  1,
+  command_play_scene  =  2,
+  command_set_scene   =  3,
+  command_rf_send     =  4, 
+  command_ir_send     =  5,
+  command_scene_pause =  9,
+  command_set_on      = 10
 };
 
 // Task
 struct Task {
 public:
-    Command command;
-    Device  device;
+  Command command;
+  Device  device;
 };
 
 
--- _web/src/hauskontrol4.js
+++ _web/src/hauskontrol4.js
@@ -130,4 +130,4 @@ const ui={
 
 
 //setup
-//(()=>{ui.setup();})();
+(()=>{ui.setup();})();
