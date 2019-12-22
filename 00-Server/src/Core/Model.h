f27d13de282e7ebb0d82d949b3f03d17d549f3e8
Marcelo Lara <marcelo@darkblue.tv>
2019-12-22 16:36:57 -0300

    CORE: device model updates

--- 00-Server/src/Core/Model.h
+++ 00-Server/src/Core/Model.h
@@ -1,25 +1,36 @@
 #pragma once
 #include "Arduino.h"
 
-
-
-
 ///////////////////////////////////////////////////////////////////////////
 // DEVICE
+
+//Device Type
 enum DeviceType {
-  devtype_light = 1,
-  devtype_env   = 4,
-  devtype_fan   = 2,
-  devtype_ac    = 3,
-  
+  devtype_light = 1, //light (use initializer to multiplex)
+  devtype_env   = 2, //Environment
+  devtype_fan   = 3, //fan 
+  devtype_ac    = 4, //air conditioner
+  devtype_bell  = 5  //door bell
+};
+
+//Zone where device is located
+enum Zone {
+  zone_living  = 1, 
+  zone_suite   = 2,   
+  zone_office  = 3,  
+  zone_kitchen = 4, 
+  zone_door    = 5 
 };
 
 struct Device {
 public:
   u8  id;       //channel
-  u8  node;     //node
+  u8  node;     //phisical node
   u32 payload;  //value
-  u8  type;     //device type
+  
+  DeviceType  type; //device type
+  Zone        zone; //associated room
+  String      name; //device name
 };
 
 struct Environment: Device {
@@ -28,13 +39,28 @@ struct Environment: Device {
   float hum;    //humidity
   float press;  //pressure
   float light;  //light level
-  
+
+  u8    chTemp; //temperature channel
+  u8    chHum;  //humidity
+  u8    chPress;//pressure
+  u8    chLight;//light level
+  long  ts;     //sampled data timestamp
 };
 
 struct Light: Device {
-  bool on;      //light status
-  u8   muxId;   //id of multpiplexed out
+  Light();
+  Light (u8 _muxId, u8 _mucPos){};
+protected:
+   bool on;      //light status
+   bool isMux;   //light is behind a multpiplexer
+public:
+  void turnOn();
+  void turnOff();
+private:
+  u8   muxId;    //id of multpiplexed out
+  u8   muxPos;   //id of multpiplexed out
 };
+
 
 struct Fan: Device {
   bool on;      //fan is on, if fan is off speed is 0
