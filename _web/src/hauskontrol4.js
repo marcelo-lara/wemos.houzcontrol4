0932833627108e4265d8573592691c520bd433d9
Marcelo Lara <marcelo@darkblue.tv>
2020-01-04 21:50:00 -0300

    fe: move rooms to rooms.js

--- _web/src/hauskontrol4.js
+++ _web/src/hauskontrol4.js
@@ -16,59 +16,9 @@ const ui={
     }
   },
 
-  rooms: {
-    list: [
-      {id:1, name:"office", selected: false, elem: undefined},
-      {id:2, name:"suite"},
-      {id:3, name:"living"},
-      {id:4, name:"kitchen"}
-    ],
-    
-    bind: ()=>{
-      const btn = document.querySelectorAll("div.rooms>div[room]");
-      btn.forEach((room)=>{
-        const _id=room.getAttribute("room")||-1;
-        let _room=ui.rooms.getById(_id);
-        if(_room!=undefined){
-          room.addEventListener("click", (ev)=>{ui.rooms.set(_id)}, true);
-          _room.elem = room;
-        }
-      });
-      ui.rooms.set(2);
-    },
-    
-    set: (roomId)=>{
-      let room = ui.rooms.getById(roomId);
-      if(room==undefined || room.selected==true) return;
-      room.selected=true;
-
-      //select on room menu
-      ui.rooms.list.forEach((_room)=>{
-        _room.selected=(_room.id==room.id);
-        if(_room.selected)
-          _room.elem.classList.add("on");
-        else
-          _room.elem.classList.remove("on");
-      });
-
-      //show devices
-      const panel = document.querySelector("div.panel");
-      panel.innerHTML='';
-      devices.byZone(roomId).forEach(dev=>{
-        if(dev.elem) panel.appendChild(dev.elem);
-      });
-
-    },
-    getById: (roomId)=>{
-      for (const room of ui.rooms.list) 
-        if(room.id==roomId) return room;
-      return undefined;
-    }
-  },
-
   weather: {
     setup: (data)=>{
-      ui.weather.envId=37;
+      ui.weather.envId=31;//external temp
       ui.weather.elem=document.querySelector("div.wrapper>div.weather");
       ui.weather.update();
       return data; // return promise data
@@ -90,10 +40,9 @@ const ui={
     }
     //fetch devices from api
     devices.setup()
-      .then(ui.weather.setup);
+      .then(ui.weather.setup)
+      .then(rooms.setup);
     ui.status.setup();
-    ui.rooms.bind();   //bind room buttons
-
   }
 };
 
---
+++ _web/src/rooms.js
@@ -0,0 +1,43 @@
+'use strict';
+const rooms = {
+  wrapper: undefined,
+  selected: 0,
+  list: [
+    {id:1, name:"living", selected: false, elem: undefined},
+    {id:2, name:"suite"},
+    {id:3, name:"office"},
+    {id:4, name:"kitchen"}
+  ],
+
+  setup: ()=>{
+    rooms.wrapper=document.querySelector("div.wrapper>div.rooms");
+    for (const room of rooms.list) {
+      let btn=document.createElement("div");
+      btn.className=room.name;
+      btn.innerText=room.label||room.name||"room";
+      btn.addEventListener("click", ()=>rooms.select(room.id), true);
+      room.elem=btn;
+      rooms.wrapper.appendChild(room.elem);
+    }
+  },
+  
+  select: zone=>{
+    if(zone==rooms.selected) return;
+    rooms.selected=zone;
+
+    //select new zone
+    rooms.wrapper
+      .querySelectorAll(".on")
+      .forEach(x=>x.classList.remove("on"));
+    rooms.list.find(x=>x.id==zone).elem.classList.add("on");
+
+    //place devices
+    const panel = document.querySelector("div.panel");
+    panel.innerHTML='';
+    devices.byZone(zone).forEach(dev=>{
+      if(dev.elem) 
+        panel.appendChild(dev.elem);
+    });
+}
+
+}
